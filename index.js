b3b558798af3dd26790f62edb2797797c6957ff2
Pedro Correia <pedro@eyeware.tech>
2022-04-23 23:23:45 +0100

    master: fix to the port.

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use('/', function(req,res){
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
-const port = 3000;
+const port = 80;
 server.listen(port);
 console.debug('Server listening on port ' + port);
